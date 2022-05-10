992f0543477a8fc198d3d3b7d5a25857466de174
DiogoVala <diogo.vala@ua.pt>
2022-05-10 02:35:08 +0100

    update

--- system/192.168.82.10/sys_server.py
+++ system/192.168.82.10/sys_server.py
@@ -23,10 +23,12 @@ import blob_detector as blob
 camera_resolution = (2016, 1520)
 
 # Minimum number of ArUco markers required for an acceptable initial calibration
-MinMarkerCount = 4 
+MinMarkerCount = 0
 
 # LED position data from this camera
 this_cam_data=None
+
+blob_id = 0
 
 # Returns (x,y) real world coordinates at height z.
 def getWorldCoordsAtZ(image_point, z, mtx, rmat, tvec):
@@ -55,7 +57,7 @@ def getWorldCoordsAtZ(image_point, z, mtx, rmat, tvec):
 
 # Processing pipeline for each frame
 def frame_processor(frame):
-	global this_cam_data
+	global this_cam_data, blob_id
 
 	keypoints = [] # List of detected keypoints in the frame
 	keypoints_sizes = []
@@ -80,8 +82,12 @@ def frame_processor(frame):
 			pt_y=int(pt[1])
 			# Crop frame around each estimated position
 			yuv_crop = frame[(pt_y-blob.crop_window):(pt_y+blob.crop_window), (pt_x-blob.crop_window):(pt_x+blob.crop_window)]
+
 			try:
 				mask_high = cv2.inRange(yuv_crop, blob.lower_range, blob.upper_range)
+				path=("%d.jpg" % (blob_id))
+				cv2.imwrite(path, mask_high)
+				blob_id+=1
 			except:
 				break
 
@@ -108,6 +114,8 @@ def frame_processor(frame):
 		
 		# Get projection coordinates in the real world
 		keypoint_realWorld = getWorldCoordsAtZ(keypoint, 0, cameraMatrix, rmat, tvec).tolist()
+
+		print(keypoint, keypoint_realWorld)
 
 		this_cam_data=[(keypoint_realWorld[0][0], keypoint_realWorld[1][0]), (camera_pos[0][0],camera_pos[1][0],camera_pos[2][0])]
 
@@ -157,11 +165,11 @@ def intersect(other_cam_data):
 	b1=np.array([other_cam_data[1][0], other_cam_data[1][1], other_cam_data[1][2]])
 	_,_,d=closestDistanceBetweenLines(a0,a1,b0,b1,clampAll=False,clampA0=False,clampA1=False,clampB0=False,clampB1=False)	
 
-	print(f"Server at: (%8.2f, %8.2f, %8.2f)mm" % (this_cam_data[1][0], this_cam_data[1][1], this_cam_data[1][2]) )
-	print(f"Client at: (%8.2f, %8.2f, %8.2f)mm" % (other_cam_data[1][0], other_cam_data[1][1], other_cam_data[1][2]) )
-	print(f"Target at: (%8.2f, %8.2f, %8.2f)mm" % (round(p[0][0],2), round(p[1][0],2), round(p[2][0],2)) )
-	print(f"Distance between lines at closest approach: %.fmm" % (d) )
-	print("\x1b[5A\r")
+	#print(f"Server at: (%8.2f, %8.2f, %8.2f)mm" % (this_cam_data[1][0], this_cam_data[1][1], this_cam_data[1][2]) )
+	#print(f"Client at: (%8.2f, %8.2f, %8.2f)mm" % (other_cam_data[1][0], other_cam_data[1][1], other_cam_data[1][2]) )
+	#print(f"Target at: (%8.2f, %8.2f, %8.2f)mm" % (round(p[0][0],2), round(p[1][0],2), round(p[2][0],2)) )
+	#print(f"Distance between lines at closest approach: %.fmm" % (d) )
+	#print("\x1b[5A\r")
 	#return p,d
 	#print("%.2f, %.2f, %.2f" % (round(p[0][0],2), round(p[1][0],2), round(p[2][0],2)) )
 	
@@ -171,107 +179,107 @@ def intersect(other_cam_data):
 
 def closestDistanceBetweenLines(a0,a1,b0,b1,clampAll=False,clampA0=False,clampA1=False,clampB0=False,clampB1=False):
 
-    ''' Given two lines defined by numpy.array pairs (a0,a1,b0,b1)
-        Return the closest points on each segment and their distance
-    '''
+	''' Given two lines defined by numpy.array pairs (a0,a1,b0,b1)
+		Return the closest points on each segment and their distance
+	'''
 	
-    # If clampAll=True, set all clamps to True
-    if clampAll:
-        clampA0=True
-        clampA1=True
-        clampB0=True
-        clampB1=True
+	# If clampAll=True, set all clamps to True
+	if clampAll:
+		clampA0=True
+		clampA1=True
+		clampB0=True
+		clampB1=True
 
 
-    # Calculate denomitator
-    A = a1 - a0
-    B = b1 - b0
-    magA = np.linalg.norm(A)
-    magB = np.linalg.norm(B)
-    
-    _A = A / magA
-    _B = B / magB
-    
-    cross = np.cross(_A, _B);
-    denom = np.linalg.norm(cross)**2
-    
-    
-    # If lines are parallel (denom=0) test if lines overlap.
-    # If they don't overlap then there is a closest point solution.
-    # If they do overlap, there are infinite closest positions, but there is a closest distance
-    if not denom:
-        d0 = np.dot(_A,(b0-a0))
-        
-        # Overlap only possible with clamping
-        if clampA0 or clampA1 or clampB0 or clampB1:
-            d1 = np.dot(_A,(b1-a0))
-            
-            # Is segment B before A?
-            if d0 <= 0 >= d1:
-                if clampA0 and clampB1:
-                    if np.absolute(d0) < np.absolute(d1):
-                        return a0,b0,np.linalg.norm(a0-b0)
-                    return a0,b1,np.linalg.norm(a0-b1)
-                
-                
-            # Is segment B after A?
-            elif d0 >= magA <= d1:
-                if clampA1 and clampB0:
-                    if np.absolute(d0) < np.absolute(d1):
-                        return a1,b0,np.linalg.norm(a1-b0)
-                    return a1,b1,np.linalg.norm(a1-b1)
-                
-                
-        # Segments overlap, return distance between parallel segments
-        return None,None,np.linalg.norm(((d0*_A)+a0)-b0)
-        
-    
-    
-    # Lines criss-cross: Calculate the projected closest points
-    t = (b0 - a0);
-    detA = np.linalg.det([t, _B, cross])
-    detB = np.linalg.det([t, _A, cross])
+	# Calculate denomitator
+	A = a1 - a0
+	B = b1 - b0
+	magA = np.linalg.norm(A)
+	magB = np.linalg.norm(B)
+	
+	_A = A / magA
+	_B = B / magB
+	
+	cross = np.cross(_A, _B);
+	denom = np.linalg.norm(cross)**2
+	
+	
+	# If lines are parallel (denom=0) test if lines overlap.
+	# If they don't overlap then there is a closest point solution.
+	# If they do overlap, there are infinite closest positions, but there is a closest distance
+	if not denom:
+		d0 = np.dot(_A,(b0-a0))
+		
+		# Overlap only possible with clamping
+		if clampA0 or clampA1 or clampB0 or clampB1:
+			d1 = np.dot(_A,(b1-a0))
+			
+			# Is segment B before A?
+			if d0 <= 0 >= d1:
+				if clampA0 and clampB1:
+					if np.absolute(d0) < np.absolute(d1):
+						return a0,b0,np.linalg.norm(a0-b0)
+					return a0,b1,np.linalg.norm(a0-b1)
+				
+				
+			# Is segment B after A?
+			elif d0 >= magA <= d1:
+				if clampA1 and clampB0:
+					if np.absolute(d0) < np.absolute(d1):
+						return a1,b0,np.linalg.norm(a1-b0)
+					return a1,b1,np.linalg.norm(a1-b1)
+				
+				
+		# Segments overlap, return distance between parallel segments
+		return None,None,np.linalg.norm(((d0*_A)+a0)-b0)
+		
+	
+	
+	# Lines criss-cross: Calculate the projected closest points
+	t = (b0 - a0);
+	detA = np.linalg.det([t, _B, cross])
+	detB = np.linalg.det([t, _A, cross])
 
-    t0 = detA/denom;
-    t1 = detB/denom;
+	t0 = detA/denom;
+	t1 = detB/denom;
 
-    pA = a0 + (_A * t0) # Projected closest point on segment A
-    pB = b0 + (_B * t1) # Projected closest point on segment B
+	pA = a0 + (_A * t0) # Projected closest point on segment A
+	pB = b0 + (_B * t1) # Projected closest point on segment B
 
 
-    # Clamp projections
-    if clampA0 or clampA1 or clampB0 or clampB1:
-        if clampA0 and t0 < 0:
-            pA = a0
-        elif clampA1 and t0 > magA:
-            pA = a1
-        
-        if clampB0 and t1 < 0:
-            pB = b0
-        elif clampB1 and t1 > magB:
-            pB = b1
-            
-        # Clamp projection A
-        if (clampA0 and t0 < 0) or (clampA1 and t0 > magA):
-            dot = np.dot(_B,(pA-b0))
-            if clampB0 and dot < 0:
-                dot = 0
-            elif clampB1 and dot > magB:
-                dot = magB
-            pB = b0 + (_B * dot)
-    
-        # Clamp projection B
-        if (clampB0 and t1 < 0) or (clampB1 and t1 > magB):
-            dot = np.dot(_A,(pB-a0))
-            if clampA0 and dot < 0:
-                dot = 0
-            elif clampA1 and dot > magA:
-                dot = magA
-            pA = a0 + (_A * dot)
+	# Clamp projections
+	if clampA0 or clampA1 or clampB0 or clampB1:
+		if clampA0 and t0 < 0:
+			pA = a0
+		elif clampA1 and t0 > magA:
+			pA = a1
+		
+		if clampB0 and t1 < 0:
+			pB = b0
+		elif clampB1 and t1 > magB:
+			pB = b1
+			
+		# Clamp projection A
+		if (clampA0 and t0 < 0) or (clampA1 and t0 > magA):
+			dot = np.dot(_B,(pA-b0))
+			if clampB0 and dot < 0:
+				dot = 0
+			elif clampB1 and dot > magB:
+				dot = magB
+			pB = b0 + (_B * dot)
+	
+		# Clamp projection B
+		if (clampB0 and t1 < 0) or (clampB1 and t1 > magB):
+			dot = np.dot(_A,(pB-a0))
+			if clampA0 and dot < 0:
+				dot = 0
+			elif clampA1 and dot > magA:
+				dot = magA
+			pA = a0 + (_A * dot)
 
-    
-    return pA,pB,np.linalg.norm(pA-pB)
-    
+	
+	return pA,pB,np.linalg.norm(pA-pB)
+	
 ####### MAIN ####### 
 print("Starting server camera.")
 
