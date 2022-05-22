e1a41a3ff0ecd8b97ff0be6ab85cbaa81439fd98
DiogoVala <diogo.vala@ua.pt>
2022-05-23 00:32:20 +0100

    update

--- tests/sync_test3.py
+++ tests/sync_test3.py
@@ -7,7 +7,7 @@ camera.resolution = (1280, 720)
 camera.vflip = True
 camera.contrast = 10
 
-file_name = "/home/pi/Pictures/video_" + str(time.time()) + ".h264"
+file_name = "video_" + str(time.time()) + ".h264"
 
 print("Start recording...")
 camera.start_recording(file_name)
