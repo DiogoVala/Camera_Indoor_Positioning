9f426988f4cc4f4499aa7bfa18b201d0fc904ce1
DiogoVala <diogo.vala@ua.pt>
2022-06-06 14:41:03 +0100

    update

--- tests/raspistill.py
+++ tests/raspistill.py
@@ -3,10 +3,7 @@ import subprocess as sp
 import atexit
 
 
-videoCmd = "raspividyuv -w "+str(w)+" -h "+str(h)+" --output - --timeout 0 --framerate "+str(fps)+" --nopreview -ex sports -ISO 150"
-videoCmd = videoCmd.split() # Popen requires that each parameter is a separate string
-cameraProcess = sp.Popen(videoCmd, stdout=sp.PIPE, bufsize=1)
+videoCmd = "raspistill -o ~/Camera_Indoor_Positioning/tests/cal.jpg -w 2016 -h 1520"
+cameraProcess = sp.call(videoCmd, shell=True)
 atexit.register(cameraProcess.terminate) # this closes the camera process in case the python scripts exits unexpectedly
 
-
-atexit.register(cameraProcess.terminate) # this closes the camera process in case the python scripts exits unexpectedly
