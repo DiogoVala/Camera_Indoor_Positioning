fb5004089338e09af9c4066cf36816e039d71972
DiogoVala <diogo.vala@ua.pt>
2022-06-07 16:39:42 +0100

    update

--- system/192.168.82.10/sys_server_new.py
+++ system/192.168.82.10/sys_server_new.py
@@ -239,7 +239,7 @@ def closestDistanceBetweenLines(a0,a1,b0,b1):
 print("Starting server camera.")
 
 # Initialize Socket Server
-#socket_sv = Socket_Server(intersect, cl_DataQ)
+socket_sv = Socket_Server(intersect, cl_DataQ)
 
 #time.sleep(0.4)
 
